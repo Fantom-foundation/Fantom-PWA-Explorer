e727ee688e77a20f5ffb8c070f465caa4e70b4bd
Pavel Spacil <pavel.spacil@gmail.com>
2020-03-09 16:29:38 +0100

    Move plugin setups to src/plugins directory

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {router} from "./router.js";
-import {apolloProvider} from "./apollo-provider.js";
-import i18n from './i18n';
-import './vue-timeago.js';
-import './vue-svgicon.js';
+import {router} from "@/plugins/router.js";
+import {apolloProvider} from "@/plugins/apollo-provider.js";
+import i18n from '@/plugins/i18n';
+import '@/plugins/vue-timeago.js';
+import '@/plugins/vue-svgicon.js';
 import {store} from "./store";
 import './filters.js';
 
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from "vue-router";
-import { routes } from "./routes.js";
+import { routes } from "../routes.js";
 
 Vue.use(VueRouter);
 
