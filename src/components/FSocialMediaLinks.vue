fec3f853fda02b12bf7a9377c6b35685fdbd314b
Pavel Spacil <pavel.spacil@gmail.com>
2020-06-11 11:06:02 +0200

    Update social media links

--- src/components/FSocialMediaLinks.vue
+++ src/components/FSocialMediaLinks.vue
@@ -1,6 +1,11 @@
 <template>
     <ul class="f-social-media-links no-markers">
         <li>
+            <a href="https://t.co/rUA9BE8kf2?amp=1" target="_blank" rel="nofollow" title="Fantom on Discord">
+                <icon data="@/assets/svg/social-media/discord.svg" width="24" height="24"></icon>
+            </a>
+        </li>
+        <li>
             <a href="https://twitter.com/FantomFDN" target="_blank" rel="nofollow" title="Fantom on Twitter">
                 <icon data="@/assets/svg/social-media/twitter.svg" width="24" height="24"></icon>
             </a>
@@ -13,11 +18,6 @@
         <li>
             <a href="https://github.com/Fantom-Foundation" target="_blank" rel="nofollow" title="Fantom on Github">
                 <icon data="@/assets/svg/social-media/github.svg" width="24" height="24"></icon>
-            </a>
-        </li>
-        <li>
-            <a href="https://t.me/Fantom_English" target="_blank" rel="nofollow" title="Fantom on Telegram">
-                <icon data="@/assets/svg/social-media/telegram.svg" width="24" height="24"></icon>
             </a>
         </li>
         <li>
