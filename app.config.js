a906857e091c97d9a2fae75fda6743bea28bbbe3
Pavel Spacil <pavel.spacil@gmail.com>
2020-06-12 11:52:32 +0200

    Remove one GA code
    Change api adresses

--- app.config.js
+++ app.config.js
@@ -1,42 +1,43 @@
 const appConfig = {
     // app title
-    name: 'Fantom Explorer',
+    name: "Fantom Explorer",
     // app description
-    description: 'Fantom Explorer allows you to explore and search the Fantom blockchain for transactions, addresses and blocks',
+    description:
+        "Fantom Explorer allows you to explore and search the Fantom blockchain for transactions, addresses and blocks",
     // app keywords
-    keywords: 'fantom, explorer, search, blockchain, crypto, currency',
+    keywords: "fantom, explorer, search, blockchain, crypto, currency",
     // apollo client settings
     apollo: {
         // list of providers. if one of them is unavailable, another is randomly picked
         providers: [
             {
-                http: 'https://api.fantom.rocks/api',
+                http: 'https://xapi2.fantom.network/api',
                 // for subscriptions
-                ws: ''
+                ws: '',
             },
             {
-                http: 'https://api2.fantom.rocks/api',
+                http: 'https://xapi3.fantom.network/api',
                 // for subscriptions
-                ws: ''
-            }
+                ws: '',
+            },
         ],
         // index into providers array of default provider or 'random' - takes index randomly
-        defaultProviderIndex: 'random'
+        defaultProviderIndex: "random"
     },
     // progressive web application
     usePWA: true,
     // pwa settings
     pwa: {
         // name used in pwa manifest
-        name: 'Fantom Explorer'
+        name: "Fantom Explorer"
     },
     // default options for production build
     build: {
         // output dir for production build
-        outputDir: 'dist'
+        outputDir: "dist"
     },
     // downtime threshold in seconds. downtime values less than this threshold will be displayed as 0
-    downtimeThreshold: 10,
+    downtimeThreshold: 10
 };
 
 // scss variables prepended to every scss file
