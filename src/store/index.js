de091d01e5e16bbe37488ddc1e3fa027109aa3be
Pavel Spacil <pavel.spacil@gmail.com>
2020-11-03 10:15:21 +0100

    Add and initialize `vuex-persist` plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex);
 
+const vuexPlugins = [];
+
+const vuexLocalStorage = new VuexPersist({
+    // The key to store the state on in the storage provider.
+    key: 'vuex-fantom-explorer',
+    storage: window.localStorage,
+    asyncStorage: false,
+    // Function that passes the state and returns the state with only the Objects you want to store.
+    reducer: (_state) => ({
+        darkMode: _state.darkMode,
+    }),
+});
+
+vuexPlugins.push(vuexLocalStorage.plugin);
+
 export const store = new Vuex.Store({
+    plugins: vuexPlugins,
+
     state: {
         breakpoints: {},
         tokenPrice: 0,
@@ -30,7 +48,7 @@ export const store = new Vuex.Store({
 
         /**
          * @param {object} _state
-         * @param {number} _tokenPrice
+         * @param {number} _gasPrice
          */
         setGasPrice(_state, _gasPrice) {
             _state.gasPrice = _gasPrice;
