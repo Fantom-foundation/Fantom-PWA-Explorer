e9f6e21303f0198851660d810c3c043df5b1a27c
Pavel Spacil <pavel.spacil@gmail.com>
2020-11-05 10:08:17 +0100

    Update FCryptoSymbol component

--- src/components/core/FCryptoSymbol/FCryptoSymbol.vue
+++ src/components/core/FCryptoSymbol/FCryptoSymbol.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="f-crypto-symbol">
         <span class="img" :style="{ width: imgWidth, height: imgHeight }">
-            <img v-if="token.logoUrl" :src="token.logoUrl" class="not-fluid" :alt="$defi.getTokenSymbol(token)" />
+            <icon v-if="svgIcon" :data="svgIcon" original />
+            <img v-else-if="token.logoUrl" :src="token.logoUrl" class="not-fluid" :alt="$defi.getTokenSymbol(token)" />
         </span>
-        {{ $defi.getTokenSymbol(token) }}
+        <span>{{ $defi.getTokenSymbol(token) }}</span>
     </div>
 </template>
 
 <script>
+import ftmIcon from '../../../assets/svg/tokens/FTM.svg';
+import wftmIcon from '../../../assets/svg/tokens/wFTM.svg';
+import fusdIcon from '../../../assets/svg/tokens/fUSD.svg';
+
 /**
  * Render crypto logo and name by given token.
  * Requires vue defi plugin ($defi).
@@ -38,6 +43,25 @@ export default {
         return {
             logoUrl: this.token ? this.token.logoUrl : '',
         };
+    },
+
+    computed: {
+        svgIcon() {
+            const { token } = this;
+
+            if (token) {
+                switch (token.symbol) {
+                    case 'FTM':
+                        return ftmIcon;
+                    case 'WFTM':
+                        return wftmIcon;
+                    case 'FUSD':
+                        return fusdIcon;
+                }
+            }
+
+            return null;
+        },
     },
 };
 </script>
