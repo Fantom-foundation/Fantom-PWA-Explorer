45ae5436a29734fa1ec980f4da6ed59936df9a3f
Pavel Spacil <pavel.spacil@gmail.com>
2020-12-11 13:01:12 +0100

    Create `Erc20AssetList` table
    Create tabs in `Assets` view

---
+++ src/data-tables/Erc20AssetList.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="address-asset-list-dt">
+        <f-data-table
+            :columns="columns"
+            :items="tokens"
+            first-m-v-column-width="6"
+            class="f-data-table-body-bg-color"
+        >
+            <template v-slot:column-asset="{ value, item, column }">
+                <div v-if="column" class="row no-collapse no-vert-col-padding">
+                    <div class="col-6 f-row-label">{{ column.label }}</div>
+                    <div class="col break-word">
+                        <f-crypto-symbol :token="item" />
+                    </div>
+                </div>
+                <template v-else>
+                    <f-crypto-symbol :token="item" />
+                </template>
+            </template>
+
+            <template v-slot:column-name="{ value, item, column }">
+                <div v-if="column" class="row no-collapse no-vert-col-padding">
+                    <div class="col-5 f-row-label">{{ column.label }}</div>
+                    <div class="col">
+                        <router-link :to="{name: 'asset-detail', params: {address: item.address}}" :title="value">
+                            {{ value }}
+                        </router-link>
+                    </div>
+                </div>
+                <template v-else>
+                    <router-link :to="{name: 'asset-detail', params: {address: item.address}}" :title="value">
+                        {{ value }}
+                    </router-link>
+                </template>
+            </template>
+
+            <template v-slot:column-address="{ value, item, column }">
+                <div v-if="column" class="row no-collapse no-vert-col-padding">
+                    <div class="col-5 f-row-label">{{ column.label }}</div>
+                    <div class="col">
+                        <router-link :to="{name: 'asset-detail', params: {address: item.address}}" :title="value">
+                            {{ item.address | formatHash }}
+                        </router-link>
+                    </div>
+                </div>
+                <template v-else>
+                    <router-link :to="{name: 'asset-detail', params: {address: item.address}}" :title="value">
+                        {{ item.address | formatHash }}
+                    </router-link>
+                </template>
+            </template>
+        </f-data-table>
+    </div>
+</template>
+
+<script>
+import FDataTable from '../components/core/FDataTable/FDataTable.vue';
+import FCryptoSymbol from '../components/core/FCryptoSymbol/FCryptoSymbol.vue';
+import { stringSort } from '../utils/array-sorting.js';
+import { formatNumberByLocale } from '../filters.js';
+
+export default {
+    name: 'Erc20AssetList',
+
+    components: { FCryptoSymbol, FDataTable },
+
+    data() {
+        return {
+            /** @type {[DefiToken]} */
+            tokens: [],
+            /** @type {DeFi} */
+            // defi: this.$defi,
+            columns: [
+                {
+                    name: 'asset',
+                    label: this.$t('view_assets.asset'),
+                    sortFunc: (_itemProp, _direction = 'asc') => {
+                        return (_a, _b) => {
+                            const a = _a.symbol;
+                            const b = _b.symbol;
+
+                            return (_direction === 'desc' ? -1 : 1) * stringSort(a, b);
+                        };
+                    },
+                    sortDir: 'desc',
+                    width: '140px',
+                },
+                {
+                    name: 'name',
+                    label: this.$t('view_assets.name'),
+                    // css: { textAlign: 'center' },
+                },
+                {
+                    name: 'address',
+                    label: this.$t('view_assets.hash'),
+                    // css: { textAlign: 'center' },
+                },
+                {
+                    name: 'totalSupply',
+                    label: this.$t('view_assets.total_supply'),
+                    formatter: (_value, _item) => {
+                        return formatNumberByLocale(
+                            this.$defi.fromTokenValue(_value, _item).toFixed(0), 0
+                        );
+                    },
+                },
+            ],
+        };
+    },
+
+    created() {
+        this.init();
+    },
+
+    methods: {
+        async init() {
+            const { $defi } = this;
+            const result = await Promise.all([
+                $defi.fetchERC20Tokens(),
+                $defi.init(),
+            ]);
+
+            // this.tokens = result[0].filter((_item) => _item.isActive && _item.symbol !== 'FTM');
+            this.tokens = result[0];
+
+            this.$emit('records-count', this.tokens.length);
+        },
+    },
+};
+</script>
--- src/views/Assets.vue
+++ src/views/Assets.vue
@@ -1,9 +1,31 @@
 <template>
-    <div class="narrow-container">
+    <div class="assets-view narrow-container">
         <f-view-heading>
-            <h1>{{ $t('view_assets.title') }} (ERC-20) <span v-if="dRecordsCount" class="f-records-count">({{ dRecordsCount }})</span></h1>
+            <h1>{{ $t('view_assets.title') }}</h1>
         </f-view-heading>
-        <asset-list />
+
+
+        <f-tabs>
+            <template #assets>
+                <h2>
+                    {{ $t('view_assets.fantom_finance_assets') }}
+                    <span class="f-records-count">({{ assetsRecordsCount }})</span>
+                </h2>
+            </template>
+            <template #erc20-assets>
+                <h2>
+                    {{ $t('view_assets.all_erc20_tokens') }}
+                    <span class="f-records-count">({{ erc20AssetsRecordsCount }})</span>
+                </h2>
+            </template>
+
+            <f-tab title-slot="assets">
+                <asset-list @records-count="onAssetsRecordsCount" />
+            </f-tab>
+            <f-tab title-slot="erc20-assets">
+                <erc20-asset-list @records-count="onErc20AssetsRecordsCount" />
+            </f-tab>
+        </f-tabs>
     </div>
 </template>
 
@@ -11,22 +33,38 @@
 
 import FViewHeading from "@/components/FViewHeading.vue";
 import AssetList from "@/data-tables/AssetList.vue";
+import FTabs from "../components/core/FTabs/FTabs.vue";
+import FTab from "../components/core/FTabs/FTab.vue";
+import Erc20AssetList from "../data-tables/Erc20AssetList.vue";
 
 export default {
     name: "Assets",
 
-    components: {AssetList, FViewHeading},
+    components: {Erc20AssetList, FTab, FTabs, AssetList, FViewHeading},
 
     data() {
         return {
-            dRecordsCount: 0
+            assetsRecordsCount: 0,
+            erc20AssetsRecordsCount: 0,
         }
     },
 
     methods: {
-        onRecordsCount(_num) {
-            this.dRecordsCount = _num;
-        }
+        onAssetsRecordsCount(_count) {
+            this.assetsRecordsCount = _count;
+        },
+
+        onErc20AssetsRecordsCount(_count) {
+            this.erc20AssetsRecordsCount = _count;
+        },
     }
 }
 </script>
+
+<style lang="scss">
+.assets-view {
+    .f-tabs {
+        margin-top: 8px;
+    }
+}
+</style>
