0c790e9729afab0190efe69e8f2241880597e303
Pavel Spacil <pavel.spacil@gmail.com>
2021-04-29 14:14:39 +0200

    Update apollo provider list

--- app.config.js
+++ app.config.js
@@ -14,13 +14,13 @@ const appConfig = {
                 http: 'https://xapi3.fantom.network/api',
                 // for subscriptions
                 ws: '',
-            },
+            },*/
             {
                 http: 'https://xapi4.fantom.network/api',
                 // for subscriptions
                 ws: '',
             },
-            {
+            /*{
                 http: 'https://xapi5.fantom.network/api',
                 // for subscriptions
                 ws: '',
@@ -35,7 +35,7 @@ const appConfig = {
                 // for subscriptions
                 ws: '',
             },
-            /*{
+            {
                 http: 'https://xapi8.fantom.network/api',
                 // for subscriptions
                 ws: '',
@@ -44,7 +44,7 @@ const appConfig = {
                 http: 'https://xapi9.fantom.network/api',
                 // for subscriptions
                 ws: '',
-            },*/
+            },
         ],
         // index into providers array of default provider or 'random' - takes index randomly
         defaultProviderIndex: "random"
