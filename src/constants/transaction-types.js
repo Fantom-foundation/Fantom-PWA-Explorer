87a55a19341d530ca1344f6dabf29a4da5010b33
Pavel Spacil <pavel.spacil@gmail.com>
2022-02-14 14:54:38 +0100

    Add transaction type constants

---
+++ src/constants/transaction-types.js
@@ -0,0 +1,27 @@
+// values are translation codes
+
+export const ERC20_TRANSACTION_TYPE = {
+    TRANSFER: 'transfer',
+    MINT: 'mint',
+    BURN: 'burn',
+    APPROVAL: 'approval',
+    OTHER: 'other',
+}
+
+export const ERC721_TRANSACTION_TYPE = {
+    TRANSFER: 'transfer',
+    MINT: 'mint',
+    BURN: 'burn',
+    APPROVAL: 'approval',
+    APPROVAL_FOR_ALL: 'approval_for_all',
+    OTHER: 'other',
+}
+
+export const ERC1155_TRANSACTION_TYPE = {
+    TRANSFER: 'transfer',
+    MINT: 'mint',
+    BURN: 'burn',
+    APPROVAL: 'approval',
+    APPROVAL_FOR_ALL: 'approval_for_all',
+    OTHER: 'other',
+}
